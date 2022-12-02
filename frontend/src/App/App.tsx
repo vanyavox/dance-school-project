a336a42a0bea7785f409e301003923e78738a6b5
Vanya <geo.vox95@gmail.com>
2022-12-02 15:11:25 +0300

    MainAndYandex

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -3,15 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from '../features/Footer/Footer';
 import Header from '../features/Header/Header';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import MainPage from '../features/main/MainPage';
+import Main from '../features/Main/Main';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Header />}>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<Main />} />
         </Route>
       </Routes>
       <Footer />
--- frontend/src/features/Main/Main.tsx
+++ frontend/src/features/Main/Main.tsx
@@ -1,11 +1,10 @@
+import { style } from '@mui/system';
 import React from 'react';
 import YandexMaps from '../yandexMaps/YandexMaps';
 
-function MainPage():JSX.Element {
+function MainPage(): JSX.Element {
   return (
-    <div>
-      <YandexMaps />
-    </div>
+    <YandexMaps />
   );
 }
 

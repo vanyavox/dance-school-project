054a99ca7572f9a0c5e8bb33f01ffc171ec29e06
Vanya <geo.vox95@gmail.com>
2022-12-02 17:55:29 +0300

    teacher, teacherlist css/tsx

---
+++ frontend/src/features/TeacherList/TeacherList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './TeacherList.module.css';
+
+function TeacherList(): JSX.Element {
+  return (
+    <div className={style.content__body}>
+      Teachers
+    </div>
+  );
+}
+
+export default TeacherList;

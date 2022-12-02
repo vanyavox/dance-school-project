0c8410d694586ab72585b2e31138ef7c55e48830
Ignaty_len <ignaty888@mail.ru>
2022-12-02 11:41:39 +0300

    route_and_main_init

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from '../features/main/MainPage';
 
 function App():JSX.Element {
   return (
-    <div className="App" />
+
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </div>
+
   );
 }
 
---
+++ frontend/src/features/main/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import YandexMaps from '../yandexMaps/YandexMaps';
+
+function MainPage():JSX.Element {
+  return (
+    <div>
+      <YandexMaps />
+    </div>
+  );
+}
+
+export default MainPage;

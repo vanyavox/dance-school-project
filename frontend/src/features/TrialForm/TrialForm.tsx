7f62ea0cc1e20e6054830d99d7e6b928d84e44ad
Ulyana <lizsong691@gmail.com>
2022-12-06 22:35:13 +0300

    Change input fields on trial form

--- frontend/src/features/TrialForm/TrialForm.tsx
+++ frontend/src/features/TrialForm/TrialForm.tsx
@@ -22,20 +22,16 @@ function TrialForm(): JSX.Element {
           ознакомительное занятие!
         </h1>
         <div className={style.form__div}>
-          <label htmlFor="name">Ваше имя</label>
-          <input {...register('name')} name="name" type="text" placeholder="Ваше имя" />
-          <label htmlFor="phone">Телефон для связи</label>
-          <input {...register('phone')} type="tel" name="phone" list="tel-list" placeholder="+7 (XXX) XXX-XX-XX" pattern="\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}" />
-          <label htmlFor="lesson_type">Направление</label>
+          <input {...register('name')} name="name" type="text" placeholder="Ваше имя" className={style.form_input} />
+          <input {...register('phone')} type="tel" name="phone" list="tel-list" placeholder="+7 (XXX) XXX-XX-XX" pattern="\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}" className={style.form_input} />
           <select {...register('lesson_type')} name="lesson_type" className={style.select_box}>
+          <option className={style.select_box__option} value="Выберите направление:" disabled>Выберите направление:</option>
             <option className={style.select_box__option} value="Латина">Латина</option>
             <option className={style.select_box__option} value="Стандарт">Стандарт</option>
             <option className={style.select_box__option} value="Двоеборье">Двоеборье</option>
           </select>
-          <label htmlFor="date">Выбрать дату</label>
-          <input {...register('date')} name="date" type="date" placeholder="Ваше имя" />
-          <label htmlFor="time">Выбрать время</label>
-          <input {...register('time')} name="time" type="time" placeholder="Ваше имя" />
+          <input {...register('date')} name="date" type="date" placeholder="Ваше имя" className={style.form_input} />
+          <input {...register('time')} name="time" type="time" placeholder="Ваше имя" className={style.form_input} />
         </div>
         <button className={style.btn__reg} type="submit">Записаться</button>
       </form>

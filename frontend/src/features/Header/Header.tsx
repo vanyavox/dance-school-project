521c90c0e5d7a84b17810b737679c0023943bda2
Roman <roman.shemelin31@gmail.com>
2022-12-07 10:17:33 +0300

    add style badge for avatar

--- frontend/src/features/Header/Header.tsx
+++ frontend/src/features/Header/Header.tsx
@@ -1,4 +1,6 @@
 import { Avatar } from '@mui/material';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
 import React, { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink, Outlet, useNavigate } from 'react-router-dom';
@@ -6,8 +8,39 @@ import { RootState, useAppDispatch } from '../../store';
 import { logout } from '../Registration/userSlice';
 import style from './Header.module.css';
 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: 'ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}));
+
 function Header(): JSX.Element {
   const dispatch = useAppDispatch();
+  const { image } = useSelector((state: RootState) => state.user);
+  const url = `http://localhost:4000${image}`;
 
   return (
     <>
@@ -39,11 +72,18 @@ function Header(): JSX.Element {
           </NavLink>
         </div>
         <NavLink to="/profile">
-          <Avatar
-            alt="My_profile"
-            src=""
-            sx={{ width: 56, height: 56 }}
-          />
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            variant="dot"
+          >
+            <Avatar
+              alt="My_profile"
+              src={url || ''}
+              sx={{ width: 70, height: 70 }}
+            />
+          </StyledBadge>
+
         </NavLink>
       </nav>
       <Outlet />

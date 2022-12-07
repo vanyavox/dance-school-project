f7005eb8205b79e10d9b06e01c357d25d374f714
Ulyana <lizsong691@gmail.com>
2022-12-07 12:13:46 +0300

    Edit trial form and change font-family

--- frontend/src/features/TrialForm/TrialForm.tsx
+++ frontend/src/features/TrialForm/TrialForm.tsx
@@ -20,14 +20,13 @@ function TrialForm(): JSX.Element {
   return (
     <div className={style.trial__form}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <h1 className={style.form_title}>Приходите на
-          ознакомительное занятие!
-        </h1>
         <div className={style.form__div}>
+          <h1 className={style.form_title}>Форма для записи
+          </h1>
           <input {...register('name')} name="name" type="text" placeholder="Ваше имя" className={style.form_input} />
           <input {...register('phone')} type="tel" name="phone" list="tel-list" placeholder="+7 (XXX) XXX-XX-XX" pattern="\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}" className={style.form_input} />
           <select {...register('lesson_type')} name="lesson_type" className={style.select_box}>
-          <option className={style.select_box__option} value="Выберите направление:" disabled>Выберите направление:</option>
+            <option className={style.select_box__option} value="Выберите направление:" disabled>Выберите направление:</option>
             <option className={style.select_box__option} value="Латина">Латина</option>
             <option className={style.select_box__option} value="Стандарт">Стандарт</option>
             <option className={style.select_box__option} value="Двоеборье">Двоеборье</option>
@@ -35,6 +34,7 @@ function TrialForm(): JSX.Element {
           <input {...register('date')} name="date" type="date" placeholder="Ваше имя" className={style.form_input} />
           <input {...register('time')} name="time" type="time" placeholder="Ваше имя" className={style.form_input} />
         </div>
+        <button className={style.btn__reg} type="button">Записаться</button>
       </form>
     </div>
   );

632ebddca3cd73853da414c2feb3cc8c94109e75
Vanya <geo.vox95@gmail.com>
2022-12-07 22:19:49 +0300

    directionsDone

--- frontend/src/features/Lesson/Lesson.tsx
+++ frontend/src/features/Lesson/Lesson.tsx
@@ -1,26 +1,33 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { RootState, useAppDispatch } from '../../store';
 import { initAsyncTeachers } from '../TeacherList/teacherSlice';
 import style from './lessoncss.module.css';
 import TeachersDiv from './Side_elements/TeachersDiv';
 
-function LessonForm():JSX.Element {
-  const { teachers } = useSelector((state:RootState) => state.teachers);
+function LessonForm(): JSX.Element {
+  const { teachers } = useSelector((state: RootState) => state.teachers);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const rotateBack = (): void => {
+    navigate(-1);
+  };
 
   useEffect(() => {
-      dispatch(initAsyncTeachers());
+    dispatch(initAsyncTeachers());
   }, []);
 
   return (
-  <div className={style.main__div}>
-    <h1>Расписание</h1>
-    <div className={style.form__div}>
-      <div className={style.table__main}>
-        <div className={style.table__line}>
-            <p className={style.table__teachers}>Преподаватель</p>
-            <div className={style.table__header}>
+    <>
+      <div className={style.main__div}>
+        <h1>Расписание</h1>
+        <div className={style.form__div}>
+          <div className={style.table__main}>
+            <div className={style.table__line}>
+              <p className={style.table__teachers}>Преподаватель</p>
+              <div className={style.table__header}>
                 <p className={style.table__td}>пн</p>
                 <p className={style.table__td}>вт</p>
                 <p className={style.table__td}>ср</p>
@@ -28,14 +35,16 @@ function LessonForm():JSX.Element {
                 <p className={style.table__td}>пт</p>
                 <p className={style.table__td}>сб</p>
                 <p className={style.table__td}>вс</p>
+              </div>
             </div>
+            {teachers.map((teacher) => (
+              <TeachersDiv key={teacher.id} teacher={teacher} />
+            ))}
+          </div>
         </div>
-        {teachers.map((teacher) => (
-          <TeachersDiv key={teacher.id} teacher={teacher} />
-        ))}
       </div>
-    </div>
-  </div>
+      <button onClick={rotateBack}>Назад</button>
+    </>
   );
 }
 export default LessonForm;
